868660c6f945a79ce6a863716f0154fb84b7a1df
TheHeiseDev <meizuset@gmail.com>
2023-06-11 11:42:04 +0300

    Закончил сборку стартовой конфигурации проекта

---
+++ src/utils/router/privateRouter.tsx
@@ -0,0 +1,8 @@
+// import { Navigate, Outlet } from "react-router-dom";
+// import { useAuth } from "hooks/use-auth";
+
+// export const PrivateRouter = () => {
+//   const { isAuth } = useAuth();
+
+//   return isAuth ? <Outlet /> : <Navigate to="/login" />;
+// };
