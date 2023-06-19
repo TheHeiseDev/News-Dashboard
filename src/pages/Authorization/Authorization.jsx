c0c82790e244c13dd50d4ed99fb6a4583c7b45d6
TheHeiseDev <meizuset@gmail.com>
2023-06-19 12:10:54 +0300

    Добавил в форме авторизации к полю пароль, кнопку при нажатии которого можно увидеть или скрыть пароль

--- src/pages/Authorization/Authorization.jsx
+++ src/pages/Authorization/Authorization.jsx
@@ -2,11 +2,17 @@ import styles from "./Authorization.module.scss";
 import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 import gif from "../../assets/gif/ai_animate3.gif";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
 
 export const Authorization = () => {
+  const [viewPass, setViewPass] = useState(false);
   const validationSchema = yup.object().shape({
     email: yup.string().required("Обязательное поле").email("Некорректный email"),
-    password: yup.string().required("Обязательное поле").min(6, "минимальная длина 4 символа"),
+    password: yup
+      .string()
+      .required("Обязательное поле")
+      .min(6, "минимальная длина 4 символа"),
   });
 
   return (
@@ -38,16 +44,33 @@ export const Authorization = () => {
           >
             {({ errors, touched }) => (
               <Form className={styles.form}>
-                <Field name="email" placeholder="Email" />
-                {errors.email && touched.email && (
-                  <div className={styles.error}>{errors.email}</div>
-                )}
-
-                <Field name="password" placeholder="Password" />
+                <div className={styles.inputContainerEmail}>
+                  <Field name="email" placeholder="Email" />
+                  {errors.email && touched.email && (
+                    <div className={styles.error}>{errors.email}</div>
+                  )}
+                </div>
+                <div className={styles.inputContainerPass}>
+                  {viewPass ? (
+                    <AiFillEye
+                      className={styles.eye}
+                      onClick={() => setViewPass((prev) => !prev)}
+                    />
+                  ) : (
+                    <AiFillEyeInvisible
+                      className={styles.eye}
+                      onClick={() => setViewPass((prev) => !prev)}
+                    />
+                  )}
+                </div>
+                <Field
+                  type={viewPass ? "text" : "password"}
+                  name="password"
+                  placeholder="Password"
+                />
                 {errors.password && touched.password && (
                   <div className={styles.error}>{errors.password}</div>
                 )}
-
                 <button type="submit">Войти</button>
               </Form>
             )}
